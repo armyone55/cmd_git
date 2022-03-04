022910cb8a4439fb6e5a0daaabab05cf4565503d
arm <dora@abc.com>
2022-03-04 12:40:17 +0700

    1234 && git push

--- new_login/lib/screen/registration_screen.dart
+++ new_login/lib/screen/registration_screen.dart
@@ -1,17 +1,158 @@
 import 'package:flutter/material.dart';
+import 'package:new_login/screen/home_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
-  const RegistrationScreen({ Key? key }) : super(key: key);
+  const RegistrationScreen({Key? key}) : super(key: key);
 
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  // our form key
+  final _formKey = GlobalKey<FormState>();
+  // editing controller
+  final firstNameEditingController = new TextEditingController();
+  final SecondNameEditingController = new TextEditingController();
+  final EmailEditingController = new TextEditingController();
+  final PasswordEditingController = new TextEditingController();
+  final ConfirmPasswordEditingController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final FirstNameField = TextFormField(
+      autofocus: false,
+      controller: firstNameEditingController,
+      keyboardType: TextInputType.name,
+      onSaved: (value) {
+        firstNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 12, 0),
+          hintText: "First Name",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+    final SecondNameField = TextFormField(
+      autofocus: false,
+      controller: SecondNameEditingController,
+      keyboardType: TextInputType.name,
+      onSaved: (value) {
+        SecondNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 12, 0),
+          hintText: "Second Name",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+    final EmailField = TextFormField(
+      autofocus: false,
+      controller: EmailEditingController,
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (value) {
+        EmailEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 12, 0),
+          hintText: "Email",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+    final PasswordField = TextFormField(
+      autofocus: false,
+      controller: PasswordEditingController,
+      obscureText: true,
+      onSaved: (value) {
+        PasswordEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 12, 0),
+          hintText: "Password",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+    final ConfirmPasswordField = TextFormField(
+      autofocus: false,
+      controller: ConfirmPasswordEditingController,
+      obscureText: true,
+      onSaved: (value) {
+        ConfirmPasswordEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 12, 0),
+          hintText: "Confirm Password",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+    final signupButton = Material(
+      elevation: 10,
+      borderRadius: BorderRadius.circular(20),
+      color: Colors.red,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => HomeScreen()));
+        },
+        child: Text("SignUp",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.normal)),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(121, 9, 192, 64),
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Color.fromARGB(57, 8, 236, 76),
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                        height: 150,
+                        child: new Image.asset(
+                          "asset/sml.jpg",
+                          fit: BoxFit.contain,
+                        )),
+                    SizedBox(height: 40),
+                    FirstNameField,
+                    SizedBox(height: 40),
+                    SecondNameField,
+                    SizedBox(height: 40),
+                    EmailField,
+                    SizedBox(height: 25),
+                    PasswordField,
+                    SizedBox(height: 25),
+                    ConfirmPasswordField,
+                    SizedBox(height: 25),
+                    signupButton,
+                    SizedBox(height: 25),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
